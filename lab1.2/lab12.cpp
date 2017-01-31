7e5a4aba0e7623278ded26c0703b6aba41fec546
Toshiki Yonezawa <tyonezawa001@student.butte.edu>
2017-01-31 22:53:58 +0000

    Lab12 Commit

--- lab1.2/lab12.cpp
+++ lab1.2/lab12.cpp
@@ -1,5 +1,6 @@
 //Created by: Toshiki Yonezawa
  //Created on: 01/31/2017
+  //The name of picture: The small bird on the tree
 
 #include <iostream>
 using namespace std;
@@ -7,9 +8,9 @@ using namespace std;
 int main() {
     
     cout << endl;
-    cout << "  ,   " << endl;
-    cout << " <@)  " << endl;
-    cout << " /))  " << endl;
+    cout << "**,***" << endl;
+    cout << "*<@)**" << endl;
+    cout << "*/))**" << endl;
     cout << "==#===" << endl;
     cout << endl;
     
--- lab1.2/lab122.cpp
+++ lab1.2/lab122.cpp
@@ -1,5 +1,6 @@
 //Created by: Toshiki Yonezawa
  //Created on: 01/31/2017
+  //The name of picture: Cookie man
  
 #include <iostream>
 using namespace std;
@@ -7,8 +8,8 @@ using namespace std;
 int main() {
     
     cout << endl;
-    cout << "   _   " << endl;
-    cout << " _(*)_ " << endl;
+    cout << "***_***" << endl;
+    cout << "*_(*)_*" << endl;
     cout << "(_ . _)" << endl;
     cout << " / : \\ " << endl;
     cout << "(_/ \\_)" << endl;
