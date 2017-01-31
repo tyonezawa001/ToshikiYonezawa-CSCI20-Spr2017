22ae121d6e31ee15fdf24d2687ccc68ee49863eb
Toshiki Yonezawa <tyonezawa001@student.butte.edu>
2017-01-31 22:57:49 +0000

    Lab12 commit

---
+++ lab1.2/lab123.cpp
@@ -0,0 +1,22 @@
+//Created by: Toshiki Yonezawa
+ //Created on: 01/31/2017
+  //The Name of Picture:
+  
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    cout << endl;
+    cout << " _" << endl;
+    cout << "( \\" << endl;
+    cout << " ) )" << endl;
+    cout << "( (  .-**-.  A.-.A" << endl;
+    cout << " \\ \\/      \\/ , , \\" << endl;
+    cout << "  \\   \\    =;  t  /=" << endl;
+    cout << "   \\   |**.  ',--'" << endl;
+    cout << "    / //  | ||" << endl;
+    cout << "   /_,))  |_,))" << endl;
+    cout << endl;
+    
+}
