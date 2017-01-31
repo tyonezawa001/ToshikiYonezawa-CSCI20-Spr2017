1a7371df739cb5a93eeccd935722b0ed9e4877ba
Toshiki Yonezawa <tyonezawa001@student.butte.edu>
2017-01-31 22:22:27 +0000

    Lab12 Commit

---
+++ lab1.2/lab12.cpp
@@ -0,0 +1,17 @@
+//Created by: Toshiki Yonezawa
+ //Created on: 01/31/2017
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    cout << endl;
+    cout << "  ,   " << endl;
+    cout << " <@)  " << endl;
+    cout << " /))  " << endl;
+    cout << "==#===" << endl;
+    cout << endl;
+    
+    return 0;
+}
---
+++ lab1.2/lab122.cpp
@@ -0,0 +1,18 @@
+//Created by: Toshiki Yonezawa
+ //Created on: 01/31/2017
+ 
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    cout << endl;
+    cout << "   _   " << endl;
+    cout << " _(*)_ " << endl;
+    cout << "(_ . _)" << endl;
+    cout << " / : \\ " << endl;
+    cout << "(_/ \\_)" << endl;
+    cout << endl;
+    
+    return 0;
+}
