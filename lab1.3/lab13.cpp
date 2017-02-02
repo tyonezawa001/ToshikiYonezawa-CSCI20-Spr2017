a479af0897da9fed8ca31795e59bb0821289aeeb
Toshiki Yonezawa <tyonezawa001@student.butte.edu>
2017-02-02 21:31:53 +0000

    Lab1.3 submission

--- lab1.3/lab13.cpp
+++ lab1.3/lab13.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-    int totalPopUS2017 = 324309805; // Total poplation at 2017 in the United States
+    int totalPopJan2017 = 324309805; // Total poplation at 2017 in the United States
     int rateBirthYear = 3942000; // Rate birht per year
     int rateDeathYear = 31536000 / 11; // Rate death per year  
     int rateImmigrationYear = 3153600 / 29; // Rate immigration per year
@@ -10,9 +10,9 @@ int main() {
     int yearYouWantKnow = 0;
     int totalPopUS = 0;
     
-    cout << "Please enter year which you want: ";
+    cout << "Please enter year which you want: "; // Asking to user
     cin >> yearYouWantKnow;
-    totalPopUS = totalPopUS2017 + ((yearYouWantKnow - 2017) * (rateBirthYear + rateDeathYear + rateImmigrationYear + rateNetGainYear));
+    totalPopUS = totalPopJan2017 + ((yearYouWantKnow - 2017) * (rateBirthYear + rateDeathYear + rateImmigrationYear + rateNetGainYear));
     cout << " In the year " << yearYouWantKnow << "," << " the total population will be " << totalPopUS << " persons in the United States.";
     
     return 0;
