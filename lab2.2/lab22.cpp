25082ed9ac50c210292a2e04af3af82df561e259
Toshiki Yonezawa <tyonezawa001@student.butte.edu>
2017-02-23 23:03:02 +0000

    Lab2.2 submission

---
+++ lab2.2/lab22.cpp
@@ -0,0 +1,33 @@
+/*
+ *Created by: Toshiki Yonezawa
+ *Created on: 02/23/2017
+ *This program will be randomly choosing a one number from 1 to 100. 
+ */
+
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+
+// void sructure 
+void randomGenerator(){
+    
+    srand(time(0));
+    int randomNum = (rand() % 100) + 1; // Choosing a number randomly in here
+    
+    cout << endl;
+    cout << "Your random number is " << randomNum << "." << endl; // making a sentence for output 
+    
+    return;
+    
+}
+
+// int main() structure 
+int main(){
+    
+    randomGenerator();
+    
+    return 0;
+    
+}
