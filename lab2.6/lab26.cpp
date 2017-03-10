66c5512d093bcb6a478d3c60ad7a8e07b07a4748
Toshiki Yonezawa <tyonezawa001@student.butte.edu>
2017-03-10 01:40:57 +0000

    Lab2.6 submission

--- lab2.6/lab26.cpp
+++ lab2.6/lab26.cpp
@@ -11,6 +11,7 @@ class TemperatureConverter {
        double GetTempFromKelvin() const;
        double GetTempAsCelsius() const;
        double GetTempAsFahrenheit() const;
+       void PrintTemperatures(double a, double b, double c);
        TemperatureConverter();
 };
 
@@ -46,13 +47,24 @@ double TemperatureConverter::GetTempAsFahrenheit() const {
     return ((9 * (kelvin_ - 273.15)) / 5) + 32;
 }
 
+void TemperatureConverter::PrintTemperatures(double a, double b, double c) {
+    cout << endl;
+    cout << "Kelvin: " << a << endl;
+    cout << "Celcius: " << b << endl;
+    cout << "Fahrenheit: " << c << endl;
+    cout << endl;
+    return;
+}
+
 int main() {
     
     int ask = 0;
     double kelvin = 0;
     double celsius = 0;
     double fahrenheit = 0;
-    
+    double a = 0;
+    double b = 0;
+    double c = 0;
     
     cout << endl;
     cout << "Toshiki: This program is working as temperature converter." << endl;
@@ -63,7 +75,7 @@ int main() {
     cout << "If you have kelvin data, please type 1." << endl;
     cout << "If you have celsius data, please type 2." << endl;
     cout << "If you have fahrenheit data, please type 3." << endl;
-    cout << "Data: ";
+    cout << "Data Type: ";
     cin >> ask;
     
     if (ask == 1) {
@@ -75,12 +87,13 @@ int main() {
         
         type1.SetTempFromKelvin(kelvin);
         
-        cout << endl;
-        cout << "Kelvin: " << kelvin << endl;
-        cout << "Celcius: " << type1.GetTempAsCelsius() << endl;
-        cout << "Fahrenheit: " << type1.GetTempAsFahrenheit() << endl;
-        cout << endl;
-        return 0;
+        a = kelvin;
+        
+        b = type1.GetTempAsCelsius();
+        
+        c = type1.GetTempAsFahrenheit();
+        
+        type1.PrintTemperatures(a, b, c);
     }
     
     if (ask == 2) {
@@ -92,29 +105,31 @@ int main() {
         
         type2.SetTempFromCelsius(celsius);
         
-        cout << endl;
-        cout << "Kelvin: " << type2.GetTempFromKelvin() << endl;
-        cout << "Celcius: " << celsius << endl;
-        cout << "Fahrenheit: " << type2.GetTempAsFahrenheit() << endl;
-        cout << endl;
-        return 0;
+        a = type2.GetTempFromKelvin();
+        
+        b = celsius;
+        
+        c = type2.GetTempAsFahrenheit();
+        
+        type2.PrintTemperatures(a, b, c);
     }
 
     if (ask == 3) {
         cout << endl;
-        cout << "Toshiki Please input your data as fahrenheit: ";
+        cout << "Toshiki: Please input your data as fahrenheit: ";
         cin >> fahrenheit;
         
         TemperatureConverter type3;
         
         type3.SetTempFromFahrenheit(fahrenheit);
         
-        cout << endl;
-        cout << "Kelvin: " << type3.GetTempFromKelvin() << endl;
-        cout << "Celcius: " << type3.GetTempAsCelsius() << endl;
-        cout << "Fahrenheit: " << fahrenheit << endl;
-        cout << endl;
-        return 0;
+        a = type3.GetTempFromKelvin();
+        
+        b = type3.GetTempAsCelsius();
+        
+        c = fahrenheit;
+        
+        type3.PrintTemperatures(a, b, c);
     }
     
     return 0;
