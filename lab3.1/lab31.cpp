c51e7ed16b8c15c1853f663c2f0bb643b5e40b94
Toshiki Yonezawa <tyonezawa001@student.butte.edu>
2017-03-24 04:38:36 +0000

    Lab3.1 submission

---
+++ lab3.1/lab31.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    cout << endl;
+    cout << "Welcome to Toshiki's Cellphone Company!" << endl;
+    cout << endl;
+    cout << "The program determines the best plan for you which based on your choices" << endl;
+    cout << endl;
+    cout << ""
+}
