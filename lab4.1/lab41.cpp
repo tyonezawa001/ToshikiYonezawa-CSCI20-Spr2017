e051029b8dfd038f7efdbe5c9ed1c6215020833f
Toshiki Yonezawa <tyonezawa001@student.butte.edu>
2017-04-16 21:04:37 +0000

    Lab4.1 submission

--- lab4.1/lab41.cpp
+++ lab4.1/lab41.cpp
@@ -15,7 +15,7 @@
  *----------
  *In the program, the class construct contains the 8 functions.
  *First class function exits for setting the single array.
- *User must set 10 numbers for the array.
+ *User must set 10 numbers for the array in the beginning.
  */
 
 
@@ -26,14 +26,14 @@ using namespace std;
 
 class ArrayTools {
     public:
-        int SetArray(); // the class function exist for setting the aingle array by user input.
-        int GetPrint(); // the class function is for printing 10 numbers which is contained by the array.
-        int GetMin(int min, int place); // the class function is for showing minimum value.
-        int GetMax(int min, int place); // the class function is for showing maximum value.
-        int GetSum(int sum); // the class function is for showing the total sum of the 10 numbers.
-        int GetEvenOdd(int j, int k); // the class function is for counting even and odd numbers which are contained by the array.
+        void SetArray(); // the class function exist for setting the aingle array by user input.
+        void GetPrint(); // the class function is for printing 10 numbers which is contained by the array.
+        void GetMin(int min, int place); // the class function is for showing minimum value.
+        void GetMax(int min, int place); // the class function is for showing maximum value.
+        void GetSum(int sum); // the class function is for showing the total sum of the 10 numbers.
+        void GetEvenOdd(int j, int k); // the class function is for counting even and odd numbers which are contained by the array.
         int GetSearch(int search); // the class is for searching a specific value in the array.
-        int GetSort(int j, int sort); // the classs is arranging for the 10 numbers in ascending order
+        void GetSort(int j, int sort); // the classs is arranging for the 10 numbers in ascending order
         ArrayTools(); // default constructor
     private:
         int i; // the variable is for every loops
@@ -51,7 +51,7 @@ ArrayTools::ArrayTools() {
 }
 
 // setting a single array by the array
-int ArrayTools::SetArray() {
+void ArrayTools::SetArray() {
     for (i = 0; i < 10; i++) {
         cout << "vals[" << i << "] = ";
         cin >> vals[i];
@@ -59,19 +59,19 @@ int ArrayTools::SetArray() {
 }
 
 // printing the array
-int ArrayTools::GetPrint() {
+void ArrayTools::GetPrint() {
     for (i = 0; i < 10; i++) {
         cout << "vals[" << i << "] = " << vals[i] << endl; 
     }
 }
 
 // showing the minimum value in the array
-int ArrayTools::GetMin(int min, int place) {
-    min = vals[0];
+void ArrayTools::GetMin(int min, int place) {
+    min = vals[0]; // the function sets the minimum value is in vals[0].
     for (i = 1; i < 10; i++) {
         if (vals[i] < min) {
             min = vals[i];
-            place = i;
+            place = i; // if the if statment found the minimum value in the array, the place stores in the 'place' variable.
         }
     }
     cout << endl;
@@ -79,12 +79,12 @@ int ArrayTools::GetMin(int min, int place) {
 }
 
 // showing the maximum value in the array
-int ArrayTools::GetMax(int max, int place) {
-    max = vals[0];
+void ArrayTools::GetMax(int max, int place) {
+    max = vals[0]; // the function sets the maximum value is in vals[0].
     for (i = 1; i < 10; i++) {
         if (max < vals[i]) {
             max = vals[i];
-            place = i;
+            place = i; // if the if statment found the maximum value in the array, the place stores in the 'place' variable.
         }
     }
     cout << endl;
@@ -92,7 +92,7 @@ int ArrayTools::GetMax(int max, int place) {
 }
 
 // showing the total sum fo the 10 numbers in the array
-int ArrayTools::GetSum(int sum) {
+void ArrayTools::GetSum(int sum) {
     sum = vals[0];
     for (i = 0; i < 10; i++) {
         sum += vals[i];
@@ -102,7 +102,7 @@ int ArrayTools::GetSum(int sum) {
 }
 
 // counting the even and odd numbers in the array
-int ArrayTools::GetEvenOdd(int j, int k) {
+void ArrayTools::GetEvenOdd(int j, int k) {
     for (i = 0; i < 10; i++) {
         if (vals[i]%2 == 0) {
             even[j] = vals[i];
@@ -142,7 +142,7 @@ int ArrayTools::GetSearch(int search) {
 }
 
 // arrnaging the 10 numbers in ascending order
-int ArrayTools::GetSort(int j, int sort) {
+void ArrayTools::GetSort(int j, int sort) {
     for (i = 0; i < 10; i++) {
         for ( j = i + 1; j < 10; j++) {
             if (vals[i] > vals[j]) {
@@ -168,8 +168,8 @@ int main() {
     int sum; // variable for total value of specific one. 
     int j = 0; // initialized value of j variable
     int k = 0; // initialized value of k variable
-    int search;
-    int sort;
+    int search; // variable for searcing number in the array
+    int sort; // variable for arranging the numbers in the aaray
     
     ArrayTools function; // object of class type
     
@@ -182,7 +182,7 @@ int main() {
     cout << "min – search the array between two sizes to return the location of the minimum value in that part of the array" << endl;
     cout << "max – search the array between two sizes to return the location of the maximum value in that part of the array" << endl;
     cout << "even/odd – count the number of even and odd numbers in the array" << endl;
-    cout << "search – search the array for a specified value and return the location if the value is found and -1 otherwise" << endl;
+    cout << "search – searcing specific value in the array." << endl;
     cout << "sort – check to see of the array is sorted in ascending order" << endl;    
     cout << "exit - end the program" << endl;
     cout << endl;
