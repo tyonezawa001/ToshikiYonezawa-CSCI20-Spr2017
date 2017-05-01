988141aca65d3dd51b44608e01c147503fc203eb
Toshiki Yonezawa <tyonezawa001@student.butte.edu>
2017-05-01 21:52:37 +0000

    Lab4.6 submission

--- lab4.6/lab46.cpp
+++ lab4.6/lab46.cpp
@@ -1,15 +1,24 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <sstream>
 using namespace std;
 
 int main() {
-    ifstream inFS("studentInfo.txt");
+    std::ifstream inFS;
     int student = 3;
     std::string name;
     string semester;
-    int unit = 0;
+    string data;
+    string stringUnit;
+    int unit;
     string grade;
+    string data2;
+    string stringTotalUnit;
+    int totalUnit;
+    string stringGpa;
+    double gpa;
+    
     int i = 0;
     
     cout << endl;
@@ -25,25 +34,37 @@ int main() {
     cout << endl;
     cout << "Reading data..." << endl;
     
-    for (i = 0; i < student; i++) {
-    
-        std::getline (inFS,name);
-        std::getline (inFS,semester);
+    std::getline (inFS,name);
+    std::getline (inFS,semester);
         
-        sscanf (inFS.studentInfo(), "%d,%s", &unit, grade);
+    cout << name << endl;
+    cout << semester << endl;
         
-        cout << name << endl;
-        cout << semester << endl;
+    while (data != "0") {
+        std::getline (inFS, data);
+        stringUnit = data.substr(0, data.find(' '));
+        stringstream convert(stringUnit); //object from the class stringstream
+        convert >> unit;  //the object has the value 12345 and stream it to the integer unit
+        
+        grade = data.substr(data.find(' ') + 1, data.length());
+        
         cout << unit << endl;
         cout << grade << endl;
-        
     }
     
+    std::getline (inFS, data2);
+    stringTotalUnit = data2.substr(0, data2.find(' '));
+    stringstream convert(stringTotalUnit);//object from the class stringstream
+    convert >> totalUnit;  //the object has the value 12345 and stream it to the integer totalUnit
+
+    stringGpa = data2.substr(data2.find(' ') + 1, data2.length());
+    
+    cout << totalUnit << endl;
+    cout << stringGpa << endl;
+
     cout << endl;
     cout << "Closing file studentInfo.txt..." << endl;
     inFS.close();
-    
-        
     
     return 0;
 }
