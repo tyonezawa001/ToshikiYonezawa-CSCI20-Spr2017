59ff68303bee5de82238dfdc01d96f9b813dd3fc
Toshiki Yonezawa <tyonezawa001@student.butte.edu>
2017-05-08 11:55:30 +0000

    Lab5.1 submsiion

--- lab5.1/lab51.cpp
+++ lab5.1/lab51.cpp
@@ -14,20 +14,20 @@ using namespace std;
 
 int main() {
     int nameLength = 0;
-    char * name = new char[]; // the line is wrong must be 
+    char * name = new char[]; // the line is wrong be cause it must have a variable in the "[]" for initializing.
 
     cout << "Please enter a 10 letter word or less" << endl;
     cin >> name;
 
-    char * head  = name;
-    char * tail = name;
-    nameLength = strlen(*name);
+    char * head  = name; // variable "head" points to variable "name". In here, the varibele "head" is initialized by "name".
+    char * tail = name; // variable "tail" also points to variable "name". In here, the variable "head" is initialized by "name".
+    nameLength = strlen(*name); // I htink the line may be wrong because if I want to know the word's length of name, I may use variable "name" not "*name".
 
     cout << "Your word is " << firststr << endl; // the line is wrong because varible "firstst" was not declared in this scope
 
-    if (nameLength<10)
+    if (nameLength < 10)
     {
-         while (*head != '\0')
+         while (*head != '\0') // I don't know what's this working for? 
          {
                 cout << *head;
                 head++;
@@ -40,7 +40,7 @@ int main() {
 
      cout << endl;
 
-     tail = &name[strlen(name) - 1];
+     tail = &name[strlen(name) - 1]; // now variable "tail" may have same address with variable "name". However, I confuse why "strlen" is in here?? 
 
      if (nameLength < 10)
      {
